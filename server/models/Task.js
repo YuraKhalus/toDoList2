07811d190a3eb86e6eeab163ee483961ffc1a1c2
Yurii Khalus <xalysjyra1992@gmail.com>
2026-08-23 16:25:37 +0300

    Create Schema for task

---
+++ server/models/Task.js
@@ -0,0 +1,30 @@
+const { mongoose } = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Назва завдання обов'язкова"],
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    priority: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        default: 'medium'
+    }, 
+    data: {
+        type: String,
+        default: new Date().toISOString.split('T')[0],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+module.exports = mongoose.model('tasks', taskSchema)
