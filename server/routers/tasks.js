dc007001d88ba7916f300155828e19f3be7a963d
Yurii Khalus <xalysjyra1992@gmail.com>
2026-08-25 19:24:52 +0300

    add new methods PUT and DELETE, add filters in GET method

--- server/routers/tasks.js
+++ server/routers/tasks.js
@@ -2,17 +2,43 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task.js');
 
+router.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`)
+    next();
+})
+
 router.get('/', async (req, res) => {
     try{
-        let query = {}
-        let tasks = await Task.find(query);
+        const { priority, sortBy } = req.query;
+        let filert = {}
+        if(priority){
+            filert.priority = priority;
+        }
+
+        let query = Task.find(filert);
+
+        if(sortBy === "data" || sortBy === "date") {
+            query = query.sort({data: -1 })
+        }
+        let tasks = await query;
+        
+        if(sortBy === "priority") {
+            const priorityOrder = {high: 1, medium: 2, low: 3 };
+            tasks.sort((a, b) => {
+                return (priorityOrder[a.priority] || 99) - (priorityOrder[b.priority] || 99)
+            });
+        }
 
         return res.status(200).json({
             success: true,
-            tasks: tasks
+            tasks
         })
     } catch (error) {
-
+        return res.status(500).json({
+            success: false,
+            message: "Помилка отримання завдань",
+            error: error.message
+        })
     }
 
 })
@@ -39,10 +65,69 @@ router.post('/', async (req, res) => {
     } catch(error) {
         return res.status(400).json({
             success: false,
-            message: `Помилка створення завдання ${req.body}`,
+            message: `Помилка створення завдання`,
             error: error.message,
         });
     }
+})
+router.put('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { title, description, priority, data } = req.body;
+
+        const updetedTask = await Task.findByIdAndUpdate(
+            id,
+            {title, description, priority, data},
+            {new: true, ranValidators: true}
+        );
+
+        if(!updetedTask){
+            return res.status(404).json({
+                success: false,
+                message: "Завдання не знайдено"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Завдання успішно оновлено',
+            task: updetedTask
+        })
+
+    } catch(error) {
+        return res.status(400).json({
+            success: false,
+            message: `Помилка редагування завдання`,
+            error: error.message,
+        });
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        console.log('delete method')
+        const { id } = req.params;
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if(!deletedTask){
+            return res.status(404).json({ 
+                success: false,
+                message: 'Незнайдено такої задачі'
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Завдання успишно видалено"
+        })
+
+    } catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: `Помилка видалення завдання`,
+            error: error.message,
+        });
+    }
+
 })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ connectDB();
 app.use(express.json());
 app.use('/api/tasks', taskRouter);
 
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Management');
 });
