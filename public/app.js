2832143aeefb697f9bb3d48f03efcca3ce6e0821
Yurii Khalus <xalysjyra1992@gmail.com>
2026-09-06 14:37:28 +0300

    Add js to frontend

--- public/app.js
+++ public/app.js
@@ -1,1 +1,65 @@
-alert('hello')
+const API_URL = "/api/tasks";
+
+let currentEditId = null;
+let currentFilter = 'all';
+let currentSort = '';
+
+const taskForm = document.querySelector('#taskForm');
+const taskTitle = document.querySelector('#taskTitle');
+const taskDescription = document.querySelector('#taskDescription');
+const taskPriority = document.querySelector('#taskPriority');
+const taskDate = document.querySelector('#taskDate');
+const submitBtnTask = document.querySelector('#submitBtnTask');
+const cancelEditBtn = document.querySelector('#cancelEdit')
+const taskList = document.querySelector('#taskList');
+const emptyState = document.querySelector('#emptyState');
+const filterButtons = document.querySelectorAll('.filter-btn');
+const sortSelect = document.querySelector('#sortBy');
+
+document.addEventListener('DOMContentLoaded', () => {
+    setupEventListeners();
+})
+
+
+function setupEventListeners(){
+    taskForm.addEventListener('submit', handleFormSubmit);
+}
+
+async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const taskData = {
+        title: taskTitle.value,
+        description: taskDescription.value.trim(),
+        priority: taskPriority.value,
+        data: taskDate.value 
+    };
+
+    if(currentEditId){
+        // await updateTask(currentEditId, taskData);
+    } else{
+        await createTask(taskData)
+    }
+    
+}
+
+async function createTask(taskData) {
+    try{
+        console.log(taskData);
+        
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(taskData),
+        });
+
+        const result = await response.json();
+
+
+    } catch(err){
+        console.error('Помилка стоврення', err);
+    }
+}
+
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const corse = require('cors');
 require('dotenv').config();
 
 const connectDB = require('./config/db.js');
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+app.use(corse())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
